6f668f2e760ebc1fa7809f6263e7a044535b1d71
avivday <avivday@gmail.com>
2019-07-18 12:32:27 +0300

    Change password added
    Change password added.
    
    Project is finished.
    Branch is clear.

--- BusWebAPI.WebAPI/Pages/src/components/admin-change-password.ts
+++ BusWebAPI.WebAPI/Pages/src/components/admin-change-password.ts
@@ -1,7 +1,32 @@
+import { UserService } from './../services/user-service';
+import { autoinject } from "aurelia-framework";
+import * as toastr from 'toastr';
+
+@autoinject()
 export class AdminChangePassword {    
-  message: string;
   
-  constructor() {
-    this.message = 'Hello world';
+  constructor(private userService: UserService) {}
+
+  oldPassword;
+  newPassword;
+  newPasswordConfirm;
+
+  changePassword() {
+
+    if(this.newPassword === this.newPasswordConfirm) 
+    {
+      const changePasswordDetails = {
+        OldPassword: this.oldPassword,
+        NewPassword: this.newPassword
+      };
+      
+      this.userService.changePassword(changePasswordDetails)
+      .then(_ => toastr.success("הססמא שונתה בהצלחה"))
+      .catch(_ => toastr.error("בעיה בשינוי הססמא. וודא שהססמא הישנה שהקשת נכונה. שים לב לאותיות גדולות"));
+
+    } else {
+      toastr.error("הססמאות שהקשת אינן זהות");
+    }
+
   }
 }
